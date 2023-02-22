688d8485f66f863d2ba82b98d6d3b3b647c196f8
Riley <94302477+Rilaey@users.noreply.github.com>
2023-02-22 18:12:59 -0500

    working on making nav links be highlighted when on corresponding page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../styles/App.css";
 
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="main-body">
+      <div>
+        <Header />
+      </div>
+      <form className="contact">
+        <h2 className="contact-head c-text">Contact</h2>
+        <label className="c-text">Name:</label>
+        <input
+          placeholder="Name"
+          required
+          min={4}
+          type="text"
+          className="c-text"
+        />
+        <label className="c-text">Email:</label>
+        <input placeholder="Email" required type="email" className="c-text" />
+        <label required className="c-text">
+          Message:
+        </label>
+        <textarea
+          class="form-control c-text"
+          rows="5"
+          type="text"
+          placeholder="Message"
+          required
+        ></textarea>
+        <div>
+          <input type="submit" value="Submit" className="c-text c-btn"></input>
+        </div>
+      </form>
+      <div className="main-footer">
+        <Footer />
+      </div>
+    </div>
+  );
 }
