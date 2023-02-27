675611fda9f97c2ff70026542b3fafd8e5aa4a2d
Riley <94302477+Rilaey@users.noreply.github.com>
2023-02-27 17:06:30 -0500

    fixed bug on smaller screens where nav links would slightly off the page.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,38 +5,44 @@ import { useLocation, useNavigate } from "react-router-dom";
 export default function Navigation() {
   let location = useNavigate();
 
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
 
   const [links, setLinks] = useState([
     {
       name: "About",
-      href: '/'
+      href: "/"
     },
     {
       name: "Projects",
-      href: '/projects'
+      href: "/projects"
     },
     {
       name: "Contact",
-      href: '/contact'
+      href: "/contact"
     },
     {
       name: "Resume",
-      href: '/resume'
-    },
-  ])
+      href: "/resume"
+    }
+  ]);
   return (
     <div className="nav-div">
       {links.map((link) => {
         return (
-          <a style={{
-            backgroundColor: pathname === link.href ? "black" : "none",
-            width: pathname === link.href ? "117px" : "100px",
-            borderRadius: pathname === link.href ? "15%" : "100%"
-          }} className="nav-link" onClick={() => {
-            location(link.href)
-          }}>{link.name}</a>
-        )
+          <a
+            style={{
+              backgroundColor: pathname === link.href ? "black" : "none",
+              width: pathname === link.href ? "117px" : "100px",
+              borderRadius: pathname === link.href ? "15%" : "100%"
+            }}
+            className="nav-link"
+            onClick={() => {
+              location(link.href);
+            }}
+          >
+            {link.name}
+          </a>
+        );
       })}
     </div>
   );
