7c67b88398e97e44fc377ed7e7c9ca7aed036708
Riley <94302477+Rilaey@users.noreply.github.com>
2023-03-03 19:37:21 -0500

    changed resume download

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -53,14 +53,14 @@ export default function Resume() {
   // Function will execute on click of button
   const onButtonClick = () => {
     // using Java Script method to get PDF file
-    fetch("resume-portfolio.pdf").then((response) => {
+    fetch("Revised-Resume.pdf").then((response) => {
       response.blob().then((blob) => {
         // Creating new object of PDF file
         const fileURL = window.URL.createObjectURL(blob);
         // Setting various property values
         let alink = document.createElement("a");
         alink.href = fileURL;
-        alink.download = "resume-portfolio.pdf.pdf";
+        alink.download = "Revised-Resume.pdf.pdf";
         alink.click();
       });
     });
