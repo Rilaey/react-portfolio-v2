bda28f0f03422b6da0dc5d165cf008c27a8ed5d9
Riley <94302477+Rilaey@users.noreply.github.com>
2023-03-09 09:55:42 -0500

    added functionality for contact form.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../styles/App.css";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
+
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_dw40vid",
+        "template_rpbdm2h",
+        form.current,
+        "tcvghC_602qwzMEG2"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset()
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="main-body">
       <div>
         <Header />
       </div>
-      <form className="contact">
+      <form className="contact" ref={form} onSubmit={sendEmail}>
         <h2 className="contact-head c-text">Contact</h2>
         <label className="c-text">Name:</label>
         <input
@@ -18,14 +44,22 @@ export default function Contact() {
           min={4}
           type="text"
           className="c-text input-text"
+          name="user_name"
           style={{
-            width: '50%'
+            width: "50%"
           }}
         />
         <label className="c-text">Email:</label>
-        <input placeholder="Email" required type="email" className="c-text input-text"          style={{
-            width: '50%'
-          }}/>
+        <input
+          placeholder="Email"
+          required
+          type="email"
+          className="c-text input-text"
+          name="user_email"
+          style={{
+            width: "50%"
+          }}
+        />
         <label required className="c-text">
           Message:
         </label>
@@ -34,10 +68,11 @@ export default function Contact() {
           rows="5"
           type="text"
           placeholder="Message"
+          name="message"
           required
         ></textarea>
         <div>
-          <input type="submit" value="Submit" className="c-text c-btn"></input>
+          <input type="submit" value="Send" className="c-text c-btn"></input>
         </div>
       </form>
       <div className="main-footer">
